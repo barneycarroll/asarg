5b9b39882dd07390614eeae572bcd616fa8e7932
barneycarroll <barney.carroll@gmail.com>
2015-11-04 02:46:26 +0000

    Works as a currier!

--- asarg.js
+++ asarg.js
@@ -1,3 +1,9 @@
 module.exports = function( fn ){
-	return fn( this )
+	return function(){
+		var subject = this
+		var input   = Array.prototype.slice.call( arguments )
+		var args    = [ subject ].concat( input )
+
+		return fn.apply( subject, args )
+	}
 }
