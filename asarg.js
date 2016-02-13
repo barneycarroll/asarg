206d4b24bb88e7693e6949c0e4b6c2e66c37d18f
impinball <impinball@gmail.com>
2016-02-12 19:40:46 -0500

    Optimize for small numbers of arguments

--- asarg.js
+++ asarg.js
@@ -1,10 +1,35 @@
 function asarg( fn ){
-	return function(){
-		var subject = this
-		var input   = Array.prototype.slice.call( arguments )
-		var args    = [ subject ].concat( input )
+	'use strict'
 
-		return fn.apply( subject, args )
+	return function(){
+		// Optimize the common case of a low-ish number of arguments. Engines
+		// are very good at optimizing function calls with consistent numbers of
+		// arguments.
+		switch (arguments.length) {
+		case 0: return fn.call( this, this )
+		case 1: return fn.call( this, this, arguments[ 0 ] )
+		case 2: return fn.call( this, this, arguments[ 0 ], arguments[ 1 ] )
+
+		case 3:
+			return fn.call( this, this,
+				arguments[ 0 ],
+				arguments[ 1 ],
+				arguments[ 2 ] )
+
+		case 4:
+			return fn.call( this, this,
+				arguments[ 0 ],
+				arguments[ 1 ],
+				arguments[ 2 ],
+				arguments[ 3 ] )
+
+		default:
+			var subject = this
+			var input   = Array.prototype.slice.call( arguments )
+			var args    = [ this ].concat( input )
+
+			return fn.apply( this, args )
+		}
 	}
 }
 
--- tests.js
+++ tests.js
@@ -3,10 +3,7 @@ import as   from './asarg'
 
 test( 'Exposes this as the first argument', test => {
 	test.equals(
-		as( arg =>
-			arg
-		).call( this ),
-
+		as( arg => arg ).call( this ),
 		this
 	)
 
@@ -15,23 +12,108 @@ test( 'Exposes this as the first argument', test => {
 
 test( 'Retains this reference', test => {
 	test.equals(
-		as( arg =>
-			this
-		).call( this ),
-
+		as( function ( arg ){ return this } ).call( this ),
 		this
 	)
 
 	test.end()
 } )
 
-test( 'Retains arguments shifted once to the right', function( test ){
-	const inputs = [ 'foo', 'bar' ]
+test( 'Retains 1 argument shifted once to the right', function( test ){
+	const inputs = [ 'foo' ]
+	const self = this
 
-	as( ( ...args ) => {
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
 		test.equals( inputs[ 0 ], args[ 1 ] )
 
+		test.end()
+	} ).apply( this, inputs )
+} )
+
+test( 'Retains 2 arguments shifted once to the right', function( test ){
+	const inputs = [ 'foo', 'bar' ]
+	const self = this
+
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
+		test.equals( inputs[ 0 ], args[ 1 ] )
 		test.equals( inputs[ 1 ], args[ 2 ] )
+
+		test.end()
+	} ).apply( this, inputs )
+} )
+
+test( 'Retains 3 arguments shifted once to the right', function( test ){
+	const inputs = [ 'foo', 'bar', 'baz' ]
+	const self = this
+
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
+		test.equals( inputs[ 0 ], args[ 1 ] )
+		test.equals( inputs[ 1 ], args[ 2 ] )
+		test.equals( inputs[ 2 ], args[ 3 ] )
+
+		test.end()
+	} ).apply( this, inputs )
+} )
+
+test( 'Retains 4 arguments shifted once to the right', function( test ){
+	const inputs = [ 'foo', 'bar', 'baz', 'spam' ]
+	const self = this
+
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
+		test.equals( inputs[ 0 ], args[ 1 ] )
+		test.equals( inputs[ 1 ], args[ 2 ] )
+		test.equals( inputs[ 2 ], args[ 3 ] )
+		test.equals( inputs[ 3 ], args[ 4 ] )
+
+		test.end()
+	} ).apply( this, inputs )
+} )
+
+test( 'Retains 5 arguments shifted once to the right', function( test ){
+	const inputs = [ 'foo', 'bar', 'baz', 'spam', 'eggs' ]
+	const self = this
+
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
+		test.equals( inputs[ 0 ], args[ 1 ] )
+		test.equals( inputs[ 1 ], args[ 2 ] )
+		test.equals( inputs[ 2 ], args[ 3 ] )
+		test.equals( inputs[ 3 ], args[ 4 ] )
+		test.equals( inputs[ 4 ], args[ 5 ] )
+
+		test.end()
+	} ).apply( this, inputs )
+} )
+
+test( 'Retains 10 arguments shifted once to the right', function( test ){
+	const inputs = [
+		'foo', 'bar', 'baz', 'spam', 'eggs',
+		'one', 'two', 'three', 'four', 'five'
+	]
+	const self = this
+
+	as( function ( ...args ){
+		test.equals( self, this )
+		test.equals( self, args[ 0 ] )
+		test.equals( inputs[ 0 ], args[ 1 ] )
+		test.equals( inputs[ 1 ], args[ 2 ] )
+		test.equals( inputs[ 2 ], args[ 3 ] )
+		test.equals( inputs[ 3 ], args[ 4 ] )
+		test.equals( inputs[ 4 ], args[ 5 ] )
+		test.equals( inputs[ 5 ], args[ 6 ] )
+		test.equals( inputs[ 6 ], args[ 7 ] )
+		test.equals( inputs[ 7 ], args[ 8 ] )
+		test.equals( inputs[ 8 ], args[ 9 ] )
+		test.equals( inputs[ 9 ], args[ 10 ] )
 
 		test.end()
 	} ).apply( this, inputs )
